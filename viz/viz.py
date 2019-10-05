3ac53e463b9a3375c96485bb4eb683e233a47c7f
Sandy Noble <sandy.noble@gmail.com>
2019-10-05 02:21:29 +0100

    Added pct_change'd data as a second y axis, attempting to spot changes.

--- viz/viz.py
+++ viz/viz.py
@@ -5,29 +5,38 @@ from matplotlib import pyplot
 import matplotlib.ticker as ticker
 
 column_names = ['datestamp', 'date', 'time', 'weight']
-series = read_csv('../output/datr20190926.csv', names=column_names)
-small_window = ('rolling4', 4)
-large_window = ('rolling36', 36)
+df = read_csv('../output/datr20190926.csv', names=column_names)
+small_window = {'name': 'rolling4', 'size': 4}
+large_window = {'name': 'rolling36', 'size': 36}
+events = 'events'
 
-series[small_window[0]] = series['weight'].rolling(small_window[1]).median()
-series[large_window[0]] = series['weight'].rolling(large_window[1]).median()
+df[small_window['name']] = df['weight'].rolling(small_window['size']).median()
+df[large_window['name']] = df['weight'].rolling(large_window['size']).median()
 
-series.plot(x='time', y=[small_window[0], large_window[0]])
-series.set_index('time')
+# produce a series, based on the results of the large_window median filter,
+# that looks for large changes that might be interesting.
 
-count = series['time'].count()
+df['pct_change'] = df[large_window['name']].pct_change()
+df.plot(x='time', y=[large_window['name'], 'pct_change'], secondary_y=['pct_change'])
+
+df.set_index('time')
+
+
+#  pretty printing
+count = df['time'].count()
 no_of_ticks = 24
 size_of_segment = int(count / no_of_ticks)
 
-# two lists, one is a list of indices regularly spread out across the series
+# two lists, one is a list of indices regularly spread out across the df
 indices = list()
 tick_labels = list()
 for i in range(0, no_of_ticks):
     position = (size_of_segment * i) + 42/2
     indices.append(position)
-    tick_labels.append(series['time'][position])
+    tick_labels.append(df['time'][position])
 
 pyplot.grid(True, which='major')
 pyplot.xticks(labels=tick_labels, ticks=indices, rotation='vertical')
+
 pyplot.tight_layout()
 pyplot.show()
