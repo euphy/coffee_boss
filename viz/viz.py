656817183b0c88753e83d7aa3e8caa6de1dbc045
Sandy Noble <sandy.noble@gmail.com>
2019-10-23 07:46:25 +0100

    Showing a diff, then a thresholded version of that diff. Getting a bit closer to making sense.

--- viz/viz.py
+++ viz/viz.py
@@ -9,7 +9,11 @@ from matplotlib import pyplot as plt
 import matplotlib.ticker as ticker
 
 column_names = ['datestamp', 'date', 'time', 'weight']
-df = read_csv('../output/datr20190923.csv', names=column_names, parse_dates=True, infer_datetime_format=True)
+df = read_csv('../output/datr20190911.csv', names=column_names, parse_dates=True, infer_datetime_format=True)
+df = df.append(read_csv('../output/datr20190912.csv', names=column_names, parse_dates=True, infer_datetime_format=True))
+df = df.append(read_csv('../output/datr20190913.csv', names=column_names, parse_dates=True, infer_datetime_format=True))
+df = df.append(read_csv('../output/datr20190914.csv', names=column_names, parse_dates=True, infer_datetime_format=True))
+
 df['datetime'] = pd.to_datetime(df['datestamp'])
 df.set_index('datetime')
 del df['datestamp']
@@ -23,40 +27,29 @@ events = 'events'
 df[small_window['name']] = df['weight'].rolling(small_window['size']).median()
 df[large_window['name']] = df['weight'].rolling(large_window['size']).median()
 
-# produce a series, based on the results of the large_window median filter,
-# that looks for large changes that might be interesting.
-
-df['pct_change'] = df[large_window['name']].pct_change()
-
-
 print(df)
+# produce a series, based on the results of the large_window median filter,
+
+df['pct_change'] = df[small_window['name']].pct_change()
+df['diff'] = df[small_window['name']].diff(periods=1)
+
+threshold = 1200.0
+df['thresholded'] = (df['diff'] > threshold) * 1
+# that looks for large changes that might be interesting.
+# df['diff'] = df['diff'].apply(lambda x: [ if y > threshold else 0 for y in x])
+df['high_points'] = df['diff'] > threshold
+df['high_points'] = df['high_points'].astype(float)
+
 print(df.info())
 
 # Type hinting. Not sure if this is kosher or not.
 fig1: Figure
 ax: Axes
 fig1, ax = plt.subplots()
-df.plot(x='datetime', y=[large_window['name'], small_window['name']], ax=ax)
-# fig2 = ax.twinx()
-# df.plot(x='datetime', y='pct_change', ax=ax)
-
-
-#  pretty printing
-# count = df['datetime'].count()
-# no_of_ticks = 24
-# size_of_segment = int(count / no_of_ticks)
-#
-# # two lists, one is a list of indices regularly spread out across the df
-# indices = list()
-# tick_labels = list()
-# for i in range(0, no_of_ticks):
-#     position = (size_of_segment * i) + 42/2
-#     indices.append(position)
-#     tick_labels.append(df['datetime'][position])
-#
+# df.plot(x='datetime', y=[small_window['name'], 'high_points'], secondary_y=['high_points'], ax=ax)
+df.plot(x='datetime', y=['thresholded', 'diff', small_window['name'] ], secondary_y=['thresholded'], ax=ax)
 ax.grid(True, which='major')
-# ax.set_xticks(indices)
-# ax.set_xticklabels(labels=tick_labels, rotation='vertical')
+
 
 plt.tight_layout()
 plt.show()
